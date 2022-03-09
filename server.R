cfc7d666195291d58a23a07d53acdfb03188534d
Erik Onsager <eonsag@uw.edu>
2022-03-09 13:55:48 -0800

    created first interactive page

--- server.R
+++ server.R
@@ -4,4 +4,16 @@ library(dplyr)
 
 server <- function(input, output) {
   
+  output$enrollmentPlot <- renderPlotly({
+    
+    df <- sat_data %>% mutate('Total' = Average.Score..SAT.Math. + Average.Score..SAT.Reading. + Average.Score..SAT.Writing.) %>% 
+                        rename('Math' = Average.Score..SAT.Math., 'Reading' = Average.Score..SAT.Reading., 'Writing' = Average.Score..SAT.Writing., 'Student Enrollment' = Student.Enrollment)
+    
+    enrollPlot <- df %>% ggplot(aes(x = `Student Enrollment`)) + geom_point(aes_string(y = input$user_input), stat = "identity") + labs(title = "Average SAT Scores Vs. Student Enrollment") +
+      theme(plot.title = element_text(hjust = 0.5))
+    
+    enrollPlot <- ggplotly(enrollPlot)
+    
+    return(enrollPlot)
+  })
 }
--- ui.R
+++ ui.R
@@ -2,6 +2,8 @@ library(ggplot2)
 library(plotly)
 library(bslib)
 
+sat_data <- read.csv("https://raw.githubusercontent.com/info-201b-wi22/final-project-ErikOnsager/main/scores.csv?token=GHSAT0AAAAAABQJHLQQURNL43ANC2TAQAV4YRSKOGQ")
+
 intro_tab <- tabPanel(
   "Introduction",
   fluidPage(theme = bs_theme(primary = "#091F43", font_scale = NULL, bootswatch = "cerulean"),
@@ -21,19 +23,22 @@ intro_tab <- tabPanel(
 
 
 plot1_sidebar <- sidebarPanel(
-  #selectInput("user_input", "Select Region to Plot", choices = climate_data$country)
+  selectInput("user_input", "Select SAT scores to plot", choices = c('Math', 'Reading', 'Writing', 'Total'))
 )
 
 plot1_main <- mainPanel(
-  #plotlyOutput(outputId = "____")
+  plotlyOutput(outputId = "enrollmentPlot")
 )
 
 interactive_tab_1 <- tabPanel(
-  "Chart 1",
+  "Chart 1: Enrollment",
   sidebarLayout(
     plot1_sidebar,
     plot1_main
-  )
+  ),
+  p("This chart was included to explore how student enrollment and average SAT score of a school are connected. It shows that generally as enrollment increases, SAT scores do as well.
+     Some other insights from the chart are: Reading and Writing scores tend to be very similar to each other, while Math has a weaker correlation to the others. Finally, most student enrollment falls between
+     200 and 700, with most average total SAT scores ranging from 1000 to 1400.")
 )
 
 
